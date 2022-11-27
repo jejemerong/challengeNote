965b53064e8075cd09932f750ff4baad10770ee3
jejemerong <9708jjw@gmail.com>
2022-11-27 21:22:32 +0900

    크기 지정 후 impl 에서 사용

--- programs/challengeNote/src/lib.rs
+++ programs/challengeNote/src/lib.rs
@@ -14,10 +14,29 @@ pub mod challenge_note {
 #[derive(Accounts)]
 pub struct Initialize {}
 
+
+// 트위터 계정 struct 를 만들어준다.
 #[account]
 pub struct Tweet {
     pub author: Pubkey,
     pub timestamp: i64,
     pub topic: String,
     pub content: String,
+}
+
+// 크기를 하나씩 지정해줌.
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
+
+// struct 속성에 크기 지정 연결!
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
 }
